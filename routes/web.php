a32bdddc1559867bbbbbd06206c203652c3c7668
sherzot <102805587+sherzot@users.noreply.github.com>
2024-09-29 15:43:07 +0900

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', function () {
-    return view('index');
-});
+
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+
+Route::get('/', [PageController::class, 'main']);
